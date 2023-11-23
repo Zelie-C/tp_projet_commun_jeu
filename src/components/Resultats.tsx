b6795c84bd2b2986858ff80e2cde4e72a3c9f6fd
Zelie-C <zelie.chemin@gmail.com>
2023-11-23 13:44:31 +0100

    composant Resultats

---
+++ src/components/Resultats.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+const Resultats = (playerName: string) => {
+    
+    const [allResults, setAllResults] = useState<number[]>([])
+
+    useEffect(
+        () => {
+
+            const localResults: number[] = JSON.parse(localStorage.getItem(playerName)!)
+            if (localResults.length > 0) {
+                setAllResults(localResults)
+            }
+        }, [])
+
+    return (
+        <>
+            <div>
+                <h2>Vos résultats</h2>
+                {allResults.length > 0 ? allResults.map(
+                    (score) => (
+                       <div className="score-result-page">{score}</div> 
+                    ))
+                : <div>Vous n'avez pas de score</div>}
+            </div>
+        </>
+    )
+}
+
+export default Resultats
