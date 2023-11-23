b6fa0c4f6db952cfefd3c86f71972f9696272562
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-11-23 14:20:02 +0100

    utilisation des proprietes pour que les etats soient dans le composant app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useCallback, useState } from 'react';
 import './App.css'
 import Compteur from './Compteur'
 
 function App() {
+  let [starttime, setStarttime] = useState(0);
+  let [endtime, setEndtime] = useState(0);
+  let [start, setStart] = useState(false);
+
+  const handleChangeStartTime = (newValue: number) => {
+    setStarttime(newValue)
+  }
+
+  const handleChangeEndTime = (newValue: number) => {
+    setEndtime(newValue)
+  }
+
+  const handleChangeStart = (newValue: boolean) => {
+    setStart(newValue)
+  }
 
   return (
     <>
       <div>
-        <Compteur />
+        <Compteur startOn={start} onChangeStartTime={handleChangeStartTime} onChangeEndTime={handleChangeEndTime} onChangeStart={handleChangeStart} />
+        
       </div>
     </>
   )
